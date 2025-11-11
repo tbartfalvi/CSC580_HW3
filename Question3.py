9cfc75eb7b24f5593a18cf20ef4447cfb5e3cc8e
tbartfalvi79 <98202640+tbartfalvi@users.noreply.github.com>
2025-11-10 22:05:41 -0700

    Update Question3.py

--- Question3.py
+++ Question3.py
@@ -7,3 +7,5 @@ from math import log, exp
 from scipy.special import gammaln  # for log factorial via gammaln(n+1)
 
 language_id_files = 'data/languageID'
+# I could not figure this question out in python.  I attempted to use ChatGPT, 
+# but I still could not do it correctly.
